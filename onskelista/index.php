abb7e5a352506e38c290bc328568c4f0811efdb9
Jakob Lindskog <jakob.lindskog@gmail.com>
2016-11-30 18:49:51 +0100

    Update

--- onskelista/index.php
+++ onskelista/index.php
@@ -7,19 +7,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#d40606">
     <link rel="icon" href="http://heartlandfamilyservice.org/wp-content/themes/hfs/img/icon-wishlist.png">
-    <title>Min önskelista</title>
+    <title>Jakobs önskelista</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
 </head>
 
 <body>
     <div class="wrapper">
-        <h1>Min önskelista</h1>
+        <h1>Jakobs önskelista</h1>
+        <ul>
         <?php
-        
-        $wishes = ["Wonder baum",""]
-        
+        $wishes = ["Fred på jorden","Kul tshirt","Nespresso-maskin (ej viktigt att den e ny eller dyr)","Wunderbaum (äpplesmak! Viktigt)","Kalsingar","Jacka? (ej så viktigt, endast förslag)"];
+        foreach($wishes as $w){
+            ?>
+            <li><?= $w ?></li>
+        <?php
+        }
         ?>
+            
+            </ul>
     </div>
 </body>
 
