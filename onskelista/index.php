dd2dd8100535bd4029a5b00e68ad1f5861910b22
Jakob Lindskog <jakob.lindskog@gmail.com>
2016-11-30 18:30:14 +0100

    Init

---
+++ onskelista/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!--Charset and viewport optimization-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#d40606">
+    <link rel="icon" href="http://heartlandfamilyservice.org/wp-content/themes/hfs/img/icon-wishlist.png">
+    <title>Min önskelista</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet">
+</head>
+
+<body>
+    <div class="wrapper">
+        <h1>Min önskelista</h1>
+        <?php
+        
+        $wishes = ["Wonder baum",""]
+        
+        ?>
+    </div>
+</body>
+
+</html>
