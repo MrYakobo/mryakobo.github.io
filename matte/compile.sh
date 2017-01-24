dfe73f2d14acd356a743d2b532a65e9ed40df82b
MrYakobo <jakob.lindskog@gmail.com>
2017-01-24 16:49:27 +0100

    Removed matte-subdomain because of copyright errors. Will reupload when reworked.

--- matte/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-CWD="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-for d in $(find -maxdepth 1 -type d)
-do
-  cd $CWD
-  #Do something, the directory is accessible with $d:
-  cd $d
-  php newindex.php > index.html
-done
